3b5bd72c4c984ca41f88606d9a443ac3855d81af
sigma390 <sigmacoder390@gmail.com>
2026-09-15 22:13:13 +0530

    feat: implement bet collection and balance tracking in main loop

--- main.go
+++ main.go
@@ -17,7 +17,41 @@ func getName() string {
 	return name
 }
 
+func getBet(balance uint) uint {
+
+	betAmount := uint(0)
+
+	for true {
+		fmt.Printf("Hey Enter an amount to place a Bet\n")
+		_, err := fmt.Scan(&betAmount)
+		if err != nil {
+			fmt.Printf("Please enter a valid amount", err)
+			continue
+		}
+		if betAmount > balance {
+			fmt.Printf("You have insufficient balance\n")
+			return 0
+		} else {
+			break
+		}
+	}
+	fmt.Printf("You have selected %d as betting Amount\n", betAmount)
+	return betAmount
+
+}
+
 func main() {
 	var name string = getName()
+	var balance uint = 200
+
+	for balance > 0 {
+		bet := getBet((balance))
+		if bet == 0 {
+			break
+		}
+		balance -= bet
+		fmt.Printf("Your balance after betting is %d\n", balance)
+	}
+
 	fmt.Printf("Hello %s\n", name)
 }
